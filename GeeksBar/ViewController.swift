a7b20d56b8acd0333098a1f282704ce5fde923a1
Adv1sery <abdylda.sovetbekov@gmail.com>
2024-04-04 19:35:37 +0600

    Initial Commit

---
+++ GeeksBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeeksBar
+//
+//  Created by Abdylda Mamashev on 4/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
